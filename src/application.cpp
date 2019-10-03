c56dede33c578d1a22da795d80fef3a361959a89
OscarSubiP <44462674+OscarSubiP@users.noreply.github.com>
2019-10-03 14:50:03 +0200

    src

---
+++ src/application.cpp
@@ -0,0 +1,216 @@
+#include "application.h"
+#include "utils.h"
+#include "mesh.h"
+#include "texture.h"
+#include "volume.h"
+#include "fbo.h"
+#include "shader.h"
+#include "input.h"
+#include "animation.h"
+#include "extra/hdre.h"
+#include "includes.h"
+
+#include <cmath>
+
+Application* Application::instance = NULL;
+Camera* Application::camera = nullptr;
+
+Application::Application(int window_width, int window_height, SDL_Window* window)
+{
+	this->window_width = window_width;
+	this->window_height = window_height;
+	this->window = window;
+	instance = this;
+	must_exit = false;
+	render_debug = true;
+	render_wireframe = false;
+
+	fps = 0;
+	frame = 0;
+	time = 0.0f;
+	elapsed_time = 0.0f;
+	mouse_locked = false;
+
+	// OpenGL flags
+	glEnable( GL_CULL_FACE ); //render both sides of every triangle
+	glEnable( GL_DEPTH_TEST ); //check the occlusions using the Z buffer
+
+	// Create camera
+	camera = new Camera();
+	camera->lookAt(Vector3(15.f, 15.0f, 25.f), Vector3(0.f, 0.0f, 0.f), Vector3(0.f, 1.f, 0.f));
+	camera->setPerspective(45.f,window_width/(float)window_height,0.1f,10000.f);
+
+	// Create scene node 
+	SceneNode * node = new SceneNode("Rendered node");
+	root.push_back(node);
+
+	// Set mesh and manipulate model matrix
+	node->mesh = Mesh::Get("data/meshes/box.ASE.mbin");
+	node->model.setScale(0.1, 0.1, 0.1);
+
+	// Create node material
+	StandardMaterial * material = new StandardMaterial();
+	node->material = material;
+
+	// Manipulate material
+	material->color = vec4(0.5, 0.0, 0.0, 1.0);
+	/*
+	material->texture = Texture::Get("data/textures/texture.tga");
+	material->shader = Shader::Get("data/shaders/basic.vs", "data/shaders/texture.fs");
+	*/
+
+	
+	
+	// Create scene node 
+	SceneNode * new_node = new SceneNode("Rendered new node");
+	root.push_back(new_node);
+
+	// Set mesh and manipulate model matrix
+	new_node->mesh = Mesh::Get("data/meshes/box.ASE.mbin");
+	new_node->model.setScale(0.1, 0.1, 0.1);
+	new_node->model.translate(200, 0, 0);
+
+	Volume* volume = new Volume(32, 32, 32);
+	volume->fillNoise(2, 4, 1);
+
+	Texture* texture = new Texture();
+	texture->create3D(32, 32, GL_RED, GL_UNSIGNED_BYTE, false, volume);
+
+	// Create node material
+	VolumeMaterial * new_material = new VolumeMaterial();
+	new_node->material = new_material;
+
+	// Manipulate material
+	new_material->color = vec4(0.5, 0.0, 0.0, 1.0);
+	new_material->texture = texture;
+
+	//hide the cursor
+	SDL_ShowCursor(!mouse_locked); //hide or show the mouse
+}
+
+//what to do when the image has to be draw
+void Application::render(void)
+{
+	//set the clear color (the background color)
+	glClearColor(0.1, 0.1, 0.1, 1.0);
+
+	// Clear the window and the depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//set the camera as default
+	camera->enable();
+
+	//set flags
+	glDisable(GL_BLEND);
+	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	for (int i = 0; i < root.size(); i++) {
+		root[i]->render(camera);
+
+		if(render_wireframe)
+			root[i]->renderWireframe(camera);
+	}
+
+	//Draw the floor grid
+	if(render_debug)
+		drawGrid();
+}
+
+void Application::update(double seconds_elapsed)
+{
+	float speed = seconds_elapsed * 10; //the speed is defined by the seconds_elapsed so it goes constant
+	float orbit_speed = seconds_elapsed * 0.5;
+	
+	//example
+	float angle = (float)seconds_elapsed * 10.0f*DEG2RAD;
+	for (int i = 0; i < root.size(); i++) {
+		root[i]->model.rotate(angle, Vector3(0,1,0));
+	}
+
+	//mouse input to rotate the cam
+	if ((Input::mouse_state & SDL_BUTTON_LEFT && !ImGui::IsAnyWindowHovered() 
+		&& !ImGui::IsAnyItemHovered() && !ImGui::IsAnyItemActive())) //is left button pressed?
+	{
+		camera->orbit(-Input::mouse_delta.x * orbit_speed, Input::mouse_delta.y * orbit_speed);
+	}
+
+	//async input to move the camera around
+	if (Input::isKeyPressed(SDL_SCANCODE_LSHIFT)) speed *= 10; //move faster with left shift
+	if (Input::isKeyPressed(SDL_SCANCODE_W) || Input::isKeyPressed(SDL_SCANCODE_UP)) camera->move(Vector3(0.0f, 0.0f, 1.0f) * speed);
+	if (Input::isKeyPressed(SDL_SCANCODE_S) || Input::isKeyPressed(SDL_SCANCODE_DOWN)) camera->move(Vector3(0.0f, 0.0f,-1.0f) * speed);
+	if (Input::isKeyPressed(SDL_SCANCODE_A) || Input::isKeyPressed(SDL_SCANCODE_LEFT)) camera->move(Vector3(1.0f, 0.0f, 0.0f) * speed);
+	if (Input::isKeyPressed(SDL_SCANCODE_D) || Input::isKeyPressed(SDL_SCANCODE_RIGHT)) camera->move(Vector3(-1.0f, 0.0f, 0.0f) * speed);
+	if (Input::isKeyPressed(SDL_SCANCODE_SPACE)) camera->moveGlobal(Vector3(0.0f, -1.0f, 0.0f) * speed);
+	if (Input::isKeyPressed(SDL_SCANCODE_LCTRL)) camera->moveGlobal(Vector3(0.0f,  1.0f, 0.0f) * speed);
+
+	//to navigate with the mouse fixed in the middle
+	if (mouse_locked)
+		Input::centerMouse();
+}
+
+//Keyboard event handler (sync input)
+void Application::onKeyDown( SDL_KeyboardEvent event )
+{
+	switch(event.keysym.sym)
+	{
+		case SDLK_ESCAPE: must_exit = true; break; //ESC key, kill the app
+		case SDLK_F1: render_debug = !render_debug; break;
+		case SDLK_F5: Shader::ReloadAll(); break; 
+	}
+}
+
+void Application::onKeyUp(SDL_KeyboardEvent event)
+{
+}
+
+void Application::onGamepadButtonDown(SDL_JoyButtonEvent event)
+{
+
+}
+
+void Application::onGamepadButtonUp(SDL_JoyButtonEvent event)
+{
+
+}
+
+void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_MIDDLE) //middle mouse
+	{
+		mouse_locked = !mouse_locked;
+		SDL_ShowCursor(!mouse_locked);
+	}
+}
+
+void Application::onMouseButtonUp(SDL_MouseButtonEvent event)
+{
+}
+
+void Application::onMouseWheel(SDL_MouseWheelEvent event)
+{
+	ImGuiIO& io = ImGui::GetIO();
+	switch (event.type)
+	{
+		case SDL_MOUSEWHEEL:
+		{
+			if (event.x > 0) io.MouseWheelH += 1;
+			if (event.x < 0) io.MouseWheelH -= 1;
+			if (event.y > 0) io.MouseWheel += 1;
+			if (event.y < 0) io.MouseWheel -= 1;
+		}
+	}
+
+	if(!ImGui::IsAnyWindowHovered() && event.y)
+		camera->changeDistance(event.y * 0.5);
+}
+
+void Application::onResize(int width, int height)
+{
+    std::cout << "window resized: " << width << "," << height << std::endl;
+	glViewport( 0,0, width, height );
+	camera->aspect =  width / (float)height;
+	window_width = width;
+	window_height = height;
+}
+
