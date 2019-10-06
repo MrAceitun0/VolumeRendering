76d9337fb9ddfe0d9f1c06fa87b0c4ed41638e7d
MrAceitun0 <aceitunodev@gmail.com>
2019-10-06 17:49:13 +0200

    Shader fixed init values outside loop

--- data/shaders/volume.fs
+++ data/shaders/volume.fs
@@ -17,21 +17,29 @@ void main()
 {
 	//Ray
 	vec3 ray = u_local_camera_position - v_position;
-	vec3 initial_sample = (v_position + 1) / 2;
-	float v = texture3D(u_texture, initial_sample);
+	vec3 current_sample = (v_position + 1) / 2;
+	vec3 dir = normalize(ray);
 	float step = u_quality;
+	vec3 step_vector = dir * step;
+
 	vec4 color_acc = vec4(0,0,0,0); 
-	vec4 color_i = vec4(v.x, v.x, v.x, v.x); //from texture
 
 	for(int i = 0; i < 4000; i++)
 	{
+		float v = texture3D(u_texture, current_sample);
+		vec4 color_i = vec4(v.x, v.x, v.x, v.x);
+
 		color_i.rgb = color_i.rgb * color_i.a;
 		color_acc = step * color_i * (1.0 - color_acc.a) + color_acc;
-		if(color_acc.a >= 1.0) //OR outside volume
+
+		if(color_acc.a >= 1.0 || texture3D(u_texture, current_sample) == 0)
 			break;	
+
+		current_sample += step_vector;
 	}
 	
 	//Brightness Options
+	/*
 	if(1.0 < color_acc.x * u_brightness)
 		color_acc.x = 1.0;
 	else
@@ -46,7 +54,7 @@ void main()
 		color_acc.z = 1.0;
 	else
 		color_acc.z = color_acc.z * u_brightness;
-
+	*/
 	//gl_FragColor = u_color;
 	
 	//For Volume Rendering
