cddca32aa37e79509486c19e2a9ffadefd79ea8c
MrAceitun0 <aceitunodev@gmail.com>
2019-10-06 13:20:15 +0200

    Shader updated

--- data/shaders/volume.fs
+++ data/shaders/volume.fs
@@ -15,39 +15,40 @@ uniform vec4 u_color;
 
 void main()
 {
-	//Ray	
-	vec3 ray = u_local_camera_position - v_position; ???
-	vec3 initial_sample = v_position;
-	step = u_quality;
-	color_acc = vec4(0,0,0,0); ???
-	color_i = vec4(????); //from texture
-		
+	//Ray
+	vec3 ray = u_local_camera_position - v_position;
+	vec3 initial_sample = (v_position + 1) / 2;
+	float v = texture3D(u_texture, initial_sample);
+	float step = u_quality;
+	vec4 color_acc = vec4(0,0,0,0); 
+	vec4 color_i = vec4(v.x, v.x, v.x, v.x); //from texture
+
 	for(int i = 0; i < 4000; i++)
 	{
 		color_i.rgb = color_i.rgb * color_i.a;
-		color_acc = step * color_i * (1 - color_acc.a) + color_acc;
-		//if(color_acc.a >= 1 || outside volume) break;
+		color_acc = step * color_i * (1.0 - color_acc.a) + color_acc;
+		if(color_acc.a >= 1.0) //OR outside volume
+			break;	
 	}
 	
-
 	//Brightness Options
-	if(1.0 < u_color.x * u_brightness)
-		u_color.x = 1.0;
+	if(1.0 < color_acc.x * u_brightness)
+		color_acc.x = 1.0;
 	else
-		u_color.x = u_color.x * u_brightness;
+		color_acc.x = color_acc.x * u_brightness;
 
-	if(1.0< u_color.y * u_brightness)
-		u_color.y = 1.0;
+	if(1.0< color_acc.y * u_brightness)
+		color_acc.y = 1.0;
 	else
-		u_color.y = u_color.y * u_brightness;
+		color_acc.y = color_acc.y * u_brightness;
 
-	if(1.0 < u_color.z * u_brightness)
-		u_color.z = 1.0;
+	if(1.0 < color_acc.z * u_brightness)
+		color_acc.z = 1.0;
 	else
-		u_color.z = u_color.z * u_brightness;
+		color_acc.z = color_acc.z * u_brightness;
 
-	gl_FragColor = u_color;
+	//gl_FragColor = u_color;
 	
 	//For Volume Rendering
-	//gl_FragColor = color_acc;
+	gl_FragColor = color_acc;
 }
